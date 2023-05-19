234416838fc570181430155504e7387cd1341273
Francesco Monfrecola <frmonfre@e3r3p6.42roma.it>
2023-05-19 10:40:04 +0200

    Makefile auto-commit

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: frmonfre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 08:35:23 by frmonfre          #+#    #+#             */
-/*   Updated: 2023/05/19 08:46:03 by frmonfre         ###   ########.fr       */
+/*   Updated: 2023/05/19 10:39:01 by frmonfre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,37 @@ char	*find_env_var(char **envp, char *var)
 
 char	*find_exec(char **src, char **argv)
 {
-	char	*out;
-
+	ft_strlcpy(*src, *src + 5, ft_strlen_chr(*src, '\0'));
 	while (*src)
 	{
-		out = ft_strconc(*src, ft_strdup("/"));
-		out = ft_strconc(out, ft_strdup(argv[2]));
-		if (!access(out, 1))
-			break ;
+		*src = ft_strconc(*src, ft_strdup("/"));
+		*src = ft_strconc(*src, ft_strdup(argv[2]));
+		if (!access(*src, 1))
+			return (*src) ;
 		src++;
 	}
-	return (out);
+	return (NULL);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*path;
 	char	**src;
-	char	*cnc;
+	int		fd1;
+	int		fd2;
 
 	path = find_env_var(envp, "PATH");
 	src = ft_split(path, ':');
-	cnc = find_exec(src, argv);
-	if (cnc != NULL)
-		execve(cnc, argv + 2, envp);
+	path = find_exec(src, argv);
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[4], O_WRONLY, O_TRUNC, O_CREAT);
+	argv[3] = NULL;
+	if (path != NULL)
+	{
+		execve(path, argv + 2, envp);
+		free(path);
+	}
+	free_char_sstar(src);
+	close(fd1);
+	close(fd2);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: frmonfre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 08:36:04 by frmonfre          #+#    #+#             */
-/*   Updated: 2023/05/19 08:40:02 by frmonfre         ###   ########.fr       */
+/*   Updated: 2023/05/19 09:52:45 by frmonfre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,9 @@ char	**ft_split(char const *s, char c);
 char	*ft_strconc(char *s1, char *s2);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+void	free_char_sstar(char **src);
+
+char	*ft_strtrim(char const *s, char const *set);
 
 #endif
