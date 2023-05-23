9be82fcd809421715b7abaf113bb8dfb6d94e329
Francesco Monfrecola <frmonfre@e3r3p8.42roma.it>
2023-05-23 11:16:20 +0200

    Makefile auto-commit

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: frmonfre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 08:35:23 by frmonfre          #+#    #+#             */
-/*   Updated: 2023/05/23 11:10:07 by frmonfre         ###   ########.fr       */
+/*   Updated: 2023/05/23 11:16:07 by frmonfre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,17 @@ t_pipex_ist	*pipex_ist_init(int ac, char **av)
 
 	ist = (t_pipex_ist *) malloc(sizeof(t_pipex_ist));
 	if (!ist)
-		exit(1);
+	{
+		write(2, "Error: malloc has not allocated\n", 32);
+		return (NULL);
+	}
 	ist->fdout = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (ist->fdout == -1)
+	{
+		free(ist);
+		return (NULL);
+	}
 	ist->tmp = dup(STDIN_FILENO);
 	ist->ac = ac;
 	ist->fdin = -1;
@@ -86,6 +94,8 @@ int	main(int ac, char **av, char **en)
 	if (ac <= 4)
 		return (write(2, "Invalid number of arguments\n", 28));
 	ist = pipex_ist_init(ac, av);
+	if (!ist)
+		return (1);
 	launch(ist, av, en);
 	close(ist->fdout);
 	free(ist);
